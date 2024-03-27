d1f82bf80d1a86022f941e5b8aab20d4b9f5d553
rodrigomfaria <rodrigomfaria@gmail.com>
2024-03-27 17:47:52 -0300

    feat: add validations

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,9 +1,20 @@
+import { Schema } from "yup";
 import User from "../models/User";
+import * as Yup from "yup";
 
 class SessionController{
 
     async store(req, res){
+
+        const schema = Yup.object().shape({
+            email: Yup.string().required().email()
+        });
+
         const {email} = req.body;
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({error: "Falha na validação"});
+        }
 
         let user = await User.findOne({email});
 
